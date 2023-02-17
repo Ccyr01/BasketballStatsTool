e388c3e1c7d4c17006626ad5dbf6d66310914c40
Ccyr01 <82624847+Ccyr01@users.noreply.github.com>
2023-02-17 16:48:22 -0700

    redo

--- app.py
+++ app.py
@@ -1,93 +1,46 @@
 import constants
 import sys
 import pdb
-Panthers = []
-Bandits = []
-Warriors = []
+
+players = constants.PLAYERS
+teams = constants.TEAMS
+
 
 def cleaned_data():
-        fixed = {}
-        for player in constants.PLAYERS:
+        cleaned = []
+        
+        for player in players:
+            fixed = {}
             fixed['name'] = player['name']
             fixed['guardians'] = player['guardians']
             if player['experience'] == 'YES':
                 fixed['experience'] = True
             else:
                 fixed['experience'] = False
-            fixed['height'] = player['height'].split(" ")[0]
-        return fixed
-
+            fixed['height'] = int(player['height'].split(" ")[0])
+            cleaned.append(fixed)
+        return cleaned
 def balance_teams():
-    length = len(constants.PLAYERS)
-    teamSize = length/3
-
-    for player in constants.PLAYERS:
-        if len(Panthers) < teamSize:
-            Panthers.append(player)
-
-        elif len(Bandits) < teamSize:
-            Bandits.append(player)
-        else:
-            Warriors.append(player)
+    playersPerTeam = len(players)/len(teams)
+    playerNo = 1
+    for team in teams:
+        print(team)
+        for player in players:
+            if playerNo < playersPerTeam:
+                print(player['name'])
     
-
-def display_stats():
-    print("BASKETBALL TEAM STATS TOOL")
-    print("---- MENU----")
-    print("Here are your choices:")
-    print("A) Display Team Stats")
-    print("B) Quit")
-    try:
+    
         
-        option = input("Enter option: \n")
-        if option == 'b' or option == 'B':
-            exit()
-        elif option == 'a' or option == 'A':
-            teamName = int(input('''For the Panthers press 1, for the Bandits press 2,
-and for the Warriors press 3\n'''))
-        else:
-            print("Not an option")
-            exit()
-    except:
-        exit()
+            
+
+            
 
 
-    displayList =[]
-    match teamName:
-        case 1:
-            print("\nPanthers")
-            print("Team size: {}".format(len(Panthers)))
-
-            for player in Panthers:
-                for key, val in player.items():
-                    if key == 'name':
-                        displayList.append(val)
-            print(*displayList, sep = ", ")
-            print()
-        case 2:
-            print("\nBandits")
-            print("Team size: {}".format(len(Bandits)))
-
-            for player in Bandits:
-                for key, val in player.items():
-                    if key == 'name':
-                        displayList.append(val)
-            print(*displayList, sep=", ")
-            print()
-        case 3:
-            print("\nWarriors")
-            print("Team size: {}".format(len(Warriors)))
-
-            for player in Warriors:
-                for key, val in player.items():
-                    if key == 'name':
-                        displayList.append(val)
-            print(*displayList, sep = ", ")
-            print()
-        case _:
-            print("INVALID RESPONSE")
         
 if __name__ == '__main__':
     cleaned_data()
-    balance_teams()
-    display_stats()
+    balance_teams()    
+
+
+    
+    # display_stats()
