e7a0bbbb0e867ea67cfa186b2322e1385c1fa57d
Ccyr01 <82624847+Ccyr01@users.noreply.github.com>
2023-02-18 02:22:30 -0700

    balanced teams working

--- app.py
+++ app.py
@@ -22,19 +22,24 @@ def cleaned_data():
         return cleaned
 def balance_teams(players):
     playersPerTeam = len(players)/len(teams)
+    print(len(players))
     print(len(teams))
     playerNo =0
-    listPointer = -1
+    listPointer=0
     n = len(teams)
     listNeeded = [[] for x in range(n)]
     
     print(listNeeded)
-
     for player in players:
-        listPointer+=1
-        while playerNo < playersPerTeam:
-            listNeeded[listPointer].append(player)
-            playerNo+=1
+            if len(listNeeded[listPointer]) < playersPerTeam:
+                print(len(listNeeded[listPointer]))
+                listNeeded[listPointer].append(player)
+                playerNo+=1
+            else:
+                print(len(listNeeded[listPointer]))
+                listPointer+=1
+                listNeeded[listPointer].append(player)
+            
     print(listNeeded)
 
     
